f9eafe8dbd0043384d0200fb5c836bdbacba3c37
Jose Agudelo-Londoño <joseagudelol@outlook.com>
2026-07-03 19:51:29 +0000

    Update elasticsearch_indexer.py

--- indexing/elasticsearch_indexer.py
+++ indexing/elasticsearch_indexer.py
@@ -113,7 +113,7 @@ for index in indexes:
 folder_path = "folder/path"
 
 for filename in os.listdir(folder_path):
-    if filename.endswith(".es.json"):
+    if filename.endswith(".json"):
         file_path = os.path.join(folder_path, filename)
 
         with open(file_path, "r") as file:
